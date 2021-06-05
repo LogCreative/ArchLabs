ea076ec06dc592e20e7928d26590444b79313125
LogCreative <longlaoda123456@qq.com>
2021-06-05 18:49:31 +0800

    compose reset

--- lab06/lab06.srcs/sources_1/new/Top.v
+++ lab06/lab06.srcs/sources_1/new/Top.v
@@ -27,6 +27,9 @@ module Top(
 
     // TODO: reverse the assignment order
 
+
+
+
     // Instruction Fetch (IF)
     reg [31:0] PC;          // TODO: the choice of PC
     reg [31:0] IF_PC;
@@ -39,15 +42,8 @@ module Top(
 
     reg [31:0] IF_INST;
     always @(posedge clk) begin
-        if(reset) begin
-            PC <= 0;
-            IF_PC <= 0;
-            IF_INST <= 0;
-        end
-        else begin
-            IF_PC <= PC + 4;
-            IF_INST <= INST_IF;
-        end
+        IF_PC <= PC + 4;
+        IF_INST <= INST_IF;
     end
 
     // Instruction Decoding (ID)
@@ -59,7 +55,7 @@ module Top(
     wire MEM_READ_ID;
     wire MEM_TO_REG_ID;
     wire MEM_WRITE_ID;
-    wire ALU_OP_ID;
+    wire [1:0] ALU_OP_ID;
     wire ALU_SRC_ID;
     wire IMM_ID;
     wire JAL_ID;
@@ -112,7 +108,7 @@ module Top(
     reg ID_MEM_READ;
     reg ID_MEM_TO_REG;
     reg ID_MEM_WRITE;
-    reg ID_ALU_OP;
+    reg [1:0] ID_ALU_OP;
     reg ID_ALU_SRC;
     reg ID_IMM;
     reg ID_JAL;
@@ -123,44 +119,23 @@ module Top(
     reg [31:0] ID_OPAND;
     reg [31:0] ID_INST;
     always @(posedge clk) begin
-        if(reset) begin
-            ID_PC <= 0;
-            ID_READ_DATA1 <= 0;
-            ID_READ_DATA2 <= 0;
-            ID_OPAND <= 0;
-            ID_INST <= 0;
+        ID_PC <= IF_PC;
+        ID_READ_DATA1 <= READ_DATA1_ID;
+        ID_READ_DATA2 <= READ_DATA2_ID;
+        ID_OPAND <= OPAND_ID;
+        ID_INST <= IF_INST;
 
-            ID_REG_DST <= 0;
-            ID_JUMP <= 0;
-            ID_BRANCH <= 0;
-            ID_MEM_READ <= 0;
-            ID_MEM_TO_REG <= 0;
-            ID_MEM_WRITE <= 0;
-            ID_ALU_OP <= 0;
-            ID_ALU_SRC <= 0;
-            ID_IMM <= 0;
-            ID_JAL <= 0;
-            ID_REG_WRITE <= 0;
-        end
-        else begin
-            ID_PC <= IF_PC;
-            ID_READ_DATA1 <= READ_DATA1_ID;
-            ID_READ_DATA2 <= READ_DATA2_ID;
-            ID_OPAND <= OPAND_ID;
-            ID_INST <= IF_INST;
-
-            ID_REG_DST <= REG_DST_ID;
-            ID_JUMP <= JUMP_ID;
-            ID_BRANCH <= BRANCH_ID;
-            ID_MEM_READ <= MEM_READ_ID;
-            ID_MEM_TO_REG <= MEM_TO_REG_ID;
-            ID_MEM_WRITE <= MEM_WRITE_ID;
-            ID_ALU_OP <= ALU_OP_ID;
-            ID_ALU_SRC <= ALU_SRC_ID;
-            ID_IMM <= IMM_ID;
-            ID_JAL <= JAL_ID;
-            ID_REG_WRITE <= REG_WRIRE_ID;
-        end
+        ID_REG_DST <= REG_DST_ID;
+        ID_JUMP <= JUMP_ID;
+        ID_BRANCH <= BRANCH_ID;
+        ID_MEM_READ <= MEM_READ_ID;
+        ID_MEM_TO_REG <= MEM_TO_REG_ID;
+        ID_MEM_WRITE <= MEM_WRITE_ID;
+        ID_ALU_OP <= ALU_OP_ID;
+        ID_ALU_SRC <= ALU_SRC_ID;
+        ID_IMM <= IMM_ID;
+        ID_JAL <= JAL_ID;
+        ID_REG_WRITE <= REG_WRIRE_ID;
     end
 
     // Execution (EX)
@@ -204,45 +179,25 @@ module Top(
     reg [31:0] EX_READ_DATA2;
     
     always @(posedge clk) begin
-        if(reset) begin
-            EX_PC <= 0;
-            EX_BRANCH_PC <= 0;
-            EX_ZERO <= 0;
-            EX_ALU_RES <= 0;
-            EX_WRITE_REG <= 0;
+        EX_PC <= ID_PC;
+        EX_BRANCH_PC <= ID_PC + (ID_OPAND[25:0] << 2);
+        EX_ZERO <= ZERO_EX;
+        EX_ALU_RES <= ALU_RES_EX;
+        EX_WRITE_REG <= ID_JAL ? 5'b11111 : (JR ? INST[25:21] : (ID_REG_DST ? ID_INST[15:11] : ID_INST[20:16]));
 
-            EX_JUMP <= 0;
-            EX_BRANCH <= 0;
-            EX_MEM_READ <= 0;
-            EX_MEM_TO_REG <= 0;
-            EX_MEM_WRITE <= 0;
-            EX_REG_WRITE <= 0;
+        EX_JUMP <=          ID_JUMP;
+        EX_BRANCH <=        ID_BRANCH;
+        EX_MEM_READ <=      ID_MEM_READ;
+        EX_MEM_TO_REG <=    ID_MEM_TO_REG;
+        EX_MEM_WRITE <=     ID_MEM_WRITE;
+        EX_REG_WRITE <=     ID_REG_WRITE;
 
-            EX_READ_DATA2 <= 0;
-        end
-        else begin
-            EX_PC <= ID_PC;
-            EX_BRANCH_PC <= ID_PC + (ID_OPAND[25:0] << 2);
-            EX_ZERO <= ZERO_EX;
-            EX_ALU_RES <= ALU_RES_EX;
-            EX_WRITE_REG <= ID_JAL ? 5'b11111 : (JR ? INST[25:21] : (ID_REG_DST ? ID_INST[15:11] : ID_INST[20:16]));
-
-            EX_JUMP <=          ID_JUMP;
-            EX_BRANCH <=        ID_BRANCH;
-            EX_MEM_READ <=      ID_MEM_READ;
-            EX_MEM_TO_REG <=    ID_MEM_TO_REG;
-            EX_MEM_WRITE <=     ID_MEM_WRITE;
-            EX_REG_WRITE <=     ID_REG_WRITE;
-
-            EX_READ_DATA2 <=    ID_READ_DATA2;
-        end
+        EX_READ_DATA2 <=    ID_READ_DATA2;
     end
 
     // Memory (MEM)
 
-    wire PC_SRC; // TODO: connect PC
-    assign PC_SRC = EX_BRANCH & EX_ZERO; 
-
+    wire PC_SRC;
     wire READ_DATA_MEM;
 
     dataMemory DataMemory(
@@ -262,24 +217,67 @@ module Top(
     reg [31:0] MEM_WRITE_REG;
 
     always @(posedge clk ) begin
-        if(reset) begin
-            MEM_REG_WRITE <= 0;
+        MEM_REG_WRITE <= EX_REG_WRITE;
+        MEM_MEM_TO_REG <= EX_MEM_TO_REG;
 
-            MEM_READ_DATA <= 0;
-            MEM_ALU_RES <= 0;
-            MEM_WRITE_REG <= 0;
-        end
-        else begin
-            MEM_REG_WRITE <= EX_REG_WRITE;
-            MEM_MEM_TO_REG <= EX_MEM_TO_REG;
-
-            MEM_READ_DATA <= READ_DATA_MEM;
-            MEM_ALU_RES <= EX_ALU_RES;
-            MEM_WRITE_REG <= EX_WRITE_REG;
-        end
+        MEM_READ_DATA <= READ_DATA_MEM;
+        MEM_ALU_RES <= EX_ALU_RES;
+        MEM_WRITE_REG <= EX_WRITE_REG;
+        assign PC_SRC = EX_BRANCH & EX_ZERO; 
     end
 
-    // write back ...
-    // MEM_MEM_TO_REG ? MEM_READ_DATA : MEM_ALU_RES
+    // Write Back (WB)
+    wire REG_WRITE_WB;
+    wire WRITE_DATA_WB;
+
+    always @(posedge clk) begin
+        assign REG_WRITE_WB = MEM_REG_WRITE;
+        assign WRITE_DATA_WB = MEM_MEM_TO_REG ? MEM_READ_DATA : MEM_ALU_RES;
+    end
+
+    always @(reset) begin
+        PC <= 0;
+        IF_PC <= 0;
+        IF_INST <= 0;
+
+        ID_PC <= 0;
+        ID_READ_DATA1 <= 0;
+        ID_READ_DATA2 <= 0;
+        ID_OPAND <= 0;
+        ID_INST <= 0;
+
+        ID_REG_DST <= 0;
+        ID_JUMP <= 0;
+        ID_BRANCH <= 0;
+        ID_MEM_READ <= 0;
+        ID_MEM_TO_REG <= 0;
+        ID_MEM_WRITE <= 0;
+        ID_ALU_OP <= 0;
+        ID_ALU_SRC <= 0;
+        ID_IMM <= 0;
+        ID_JAL <= 0;
+        ID_REG_WRITE <= 0;
+
+        EX_PC <= 0;
+        EX_BRANCH_PC <= 0;
+        EX_ZERO <= 0;
+        EX_ALU_RES <= 0;
+        EX_WRITE_REG <= 0;
+
+        EX_JUMP <= 0;
+        EX_BRANCH <= 0;
+        EX_MEM_READ <= 0;
+        EX_MEM_TO_REG <= 0;
+        EX_MEM_WRITE <= 0;
+        EX_REG_WRITE <= 0;
+
+        EX_READ_DATA2 <= 0;
+
+        MEM_REG_WRITE <= 0;
+
+        MEM_READ_DATA <= 0;
+        MEM_ALU_RES <= 0;
+        MEM_WRITE_REG <= 0;
+    end
 
 endmodule
